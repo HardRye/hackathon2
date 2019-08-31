65cd977739d980150909ddd3f34148f54b75e651
Anna Kholod <uashop89@gmail.com>
2019-08-31 18:55:55 +0300

    init logic

--- src/js/3navigation.js
+++ src/js/3navigation.js
@@ -1,6 +1,6 @@
 const btnHome = document.querySelector('.header__link--home');
 const btnLibrary = document.querySelector('.header__link--library');
-const sectionMain = document.querySelector('.main');
+const sectionMain = document.querySelector('.home-page');
 const sectionCard = document.querySelector('.card');
 const sectionLibrary = document.querySelector('.myFilmLibraryPage__container');
 const btnFavourites = document.querySelector('.myFilmLibraryPage__buttonFavourites');
@@ -10,12 +10,20 @@ const btnRemoveFromFavourite = document.querySelector('.card-details__RemoveFrom
 const btnAddToQueue = document.querySelector('.card-details__AddToQueue');
 const btnRemoveFromQueue = document.querySelector('.card-details__RemoveFromQueue');
 const headerNav = document.querySelector('.header__nav');
+const headerLogo = document.querySelector('.header__logo');
 
 let selectFilm;
 
 const activeHomePage = () => {
   sectionCard.classList.add('non-active-section');
   sectionLibrary.classList.add('non-active-section');
+  sectionMain.classList.remove('non-active-section');
+}
+
+const activeLibraryPage = () => {
+  sectionCard.classList.add('non-active-section');
+  sectionMain.classList.add('non-active-section');
+  sectionLibrary.classList.remove('non-active-section');
 }
 
 const chooseActiveLink = evt => {
@@ -32,4 +40,6 @@ const chooseActiveLink = evt => {
 activeHomePage();
 
 btnHome.addEventListener('click', activeHomePage);
+headerLogo.addEventListener('click', activeHomePage);
+btnLibrary.addEventListener('click', activeLibraryPage);
 headerNav.addEventListener('click', chooseActiveLink);
