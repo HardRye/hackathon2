5a674bdf87ee229c04cb6d855c312b842a68890c
HardRye <nikolaimihail@gmail.com>
2019-08-31 10:44:26 +0300

    fonts added

--- src/index.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-
-import * as initialHomePage from './js/1initialHomePage.js';
-import * as searchAndPlaginationHomePage from './js/2searchAndPlaginationHomePage.js';
-import * as navigation from './js/3navigation.js';
-import * as filmDetailsPage from './js/4filmDetailsPage.js';
-import * as libraryPage from './js/5libraryPage.js'; 
