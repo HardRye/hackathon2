00dd8d3e8755b3c31f57db903fb83d7725b3b0df
HardRye <nikolaimihail@gmail.com>
2019-09-01 15:38:45 +0300

    tests

--- src/js/3navigation.js
+++ src/js/3navigation.js
@@ -1,7 +1,7 @@
 const btnHome = document.querySelector('.header__link--home');
 const btnLibrary = document.querySelector('.header__link--library');
 const sectionMain = document.querySelector('.home-page');
-const sectionCard = document.querySelector('.card');
+const sectionCard = document.querySelector('.film-card');
 const sectionLibrary = document.querySelector('main > .myFilmLibraryPage__container');
 const btnFavourites = document.querySelector('.myFilmLibraryPage__buttonFavourites');
 const btnQueue = document.querySelector('.myFilmLibraryPage__buttonQueue');
@@ -30,7 +30,7 @@ function activeDetailsPageListener(e) {
   sectionCard.classList.remove('non-active-section');
   sectionMain.classList.add('non-active-section');
   sectionLibrary.classList.add('non-active-section');
- 
+
   if (e.currentTarget.dataset.name === 'home') {
     activeDetailsPage(e.target.dataset.id, false);
   } else if (e.currentTarget.dataset.name === 'favourites') {
@@ -53,9 +53,9 @@ function activeDetailsPageListener(e) {
 }
 
 function activeDetailsPage(movieId, itsLibraryFilm) {
-  if(itsLibraryFilm) {
+  if (itsLibraryFilm) {
     selectFilm = filmsFromLocalQueue.find(item => item.id === Number(movieId));
-    if(!selectFilm) {
+    if (!selectFilm) {
       selectFilm = filmsFromLocalWatched.find(item => item.id === Number(movieId));
     }
   } else {
@@ -66,12 +66,12 @@ function activeDetailsPage(movieId, itsLibraryFilm) {
 
 function chooseActiveLink(evt) {
   evt.preventDefault();
-  if(evt.target.classList.contains('header__link')) {
+  if (evt.target.classList.contains('header__link')) {
     const currentActiveItem = headerNav.querySelector('.header__link--active');
-        if(currentActiveItem) {
-          currentActiveItem.classList.remove('header__link--active');
-        }
-        evt.target.classList.add('header__link--active');
+    if (currentActiveItem) {
+      currentActiveItem.classList.remove('header__link--active');
+    }
+    evt.target.classList.add('header__link--active');
   }
 }
 
