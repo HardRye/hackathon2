7e1594286e3f7976f13c5a819322901c304271cc
AlexxxxK <oleksandr.kozyr@outlook.com>
2019-09-01 14:54:14 +0300

    filmDetails_Alexander_1

--- src/js/3navigation.js
+++ src/js/3navigation.js
@@ -1,14 +1,24 @@
 const btnHome = document.querySelector('.header__link--home');
 const btnLibrary = document.querySelector('.header__link--library');
 const sectionMain = document.querySelector('.home-page');
-const sectionCard = document.querySelector('.card');
-const sectionLibrary = document.querySelector('main > .myFilmLibraryPage__container');
-const btnFavourites = document.querySelector('.myFilmLibraryPage__buttonFavourites');
+const sectionCard = document.querySelector('.film-card');
+const sectionLibrary = document.querySelector(
+  'main > .myFilmLibraryPage__container',
+);
+const btnFavourites = document.querySelector(
+  '.myFilmLibraryPage__buttonFavourites',
+);
 const btnQueue = document.querySelector('.myFilmLibraryPage__buttonQueue');
-const btnAddToFavourite = document.querySelector('.card-details__AddToFavourite');
-const btnRemoveFromFavourite = document.querySelector('.card-details__RemoveFromFavourite');
+const btnAddToFavourite = document.querySelector(
+  '.card-details__AddToFavourite',
+);
+const btnRemoveFromFavourite = document.querySelector(
+  '.card-details__RemoveFromFavourite',
+);
 const btnAddToQueue = document.querySelector('.card-details__AddToQueue');
-const btnRemoveFromQueue = document.querySelector('.card-details__RemoveFromQueue');
+const btnRemoveFromQueue = document.querySelector(
+  '.card-details__RemoveFromQueue',
+);
 const headerNav = document.querySelector('.header__nav');
 const headerLogo = document.querySelector('.header__logo');
 
@@ -30,13 +40,15 @@ function activeDetailsPageListener(e) {
   sectionCard.classList.remove('non-active-section');
   sectionMain.classList.add('non-active-section');
   sectionLibrary.classList.add('non-active-section');
- 
+
   if (e.currentTarget.dataset.name === 'home') {
     activeDetailsPage(e.target.dataset.id, false);
   } else if (e.currentTarget.dataset.name === 'favourites') {
     try {
       const filmsFromLocalWatched = localStorage.getItem('filmsWatched');
-      return filmsFromLocalWatched === null ? undefined : JSON.parse(filmsFromLocalWatched);
+      return filmsFromLocalWatched === null
+        ? undefined
+        : JSON.parse(filmsFromLocalWatched);
     } catch (err) {
       console.error(err);
     }
@@ -44,7 +56,9 @@ function activeDetailsPageListener(e) {
   } else if (e.currentTarget.dataset.name === 'favourites') {
     try {
       const filmsFromLocalQueue = localStorage.getItem('filmsQueue');
-      return filmsFromLocalQueue === null ? undefined : JSON.parse(filmsFromLocalQueue);
+      return filmsFromLocalQueue === null
+        ? undefined
+        : JSON.parse(filmsFromLocalQueue);
     } catch (err) {
       console.error(err);
     }
@@ -53,10 +67,12 @@ function activeDetailsPageListener(e) {
 }
 
 function activeDetailsPage(movieId, itsLibraryFilm) {
-  if(itsLibraryFilm) {
+  if (itsLibraryFilm) {
     selectFilm = filmsFromLocalQueue.find(item => item.id === Number(movieId));
-    if(!selectFilm) {
-      selectFilm = filmsFromLocalWatched.find(item => item.id === Number(movieId));
+    if (!selectFilm) {
+      selectFilm = filmsFromLocalWatched.find(
+        item => item.id === Number(movieId),
+      );
     }
   } else {
     selectFilm = renderFilms.find(item => item.id === Number(movieId));
@@ -66,12 +82,12 @@ function activeDetailsPage(movieId, itsLibraryFilm) {
 
 function chooseActiveLink(evt) {
   evt.preventDefault();
-  if(evt.target.classList.contains('header__link')) {
+  if (evt.target.classList.contains('header__link')) {
     const currentActiveItem = headerNav.querySelector('.header__link--active');
-        if(currentActiveItem) {
-          currentActiveItem.classList.remove('header__link--active');
-        }
-        evt.target.classList.add('header__link--active');
+    if (currentActiveItem) {
+      currentActiveItem.classList.remove('header__link--active');
+    }
+    evt.target.classList.add('header__link--active');
   }
 }
 
