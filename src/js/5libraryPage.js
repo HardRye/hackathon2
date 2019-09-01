309283665b98f98d5dec1ac193e2ac1f0b5bd403
Maksym Osadchuk <mosadchuk7@gmail.com>
2019-09-01 11:46:20 +0300

    logic ready v1

--- src/js/5libraryPage.js
+++ src/js/5libraryPage.js
@@ -1,4 +1,8 @@
 const films = document.querySelector('.myFilmLibraryPage__film-card');
+const favouriteButton = document.querySelector(
+  '.myFilmLibraryPage__buttonFavourites',
+);
+const queueButton = document.querySelector('.myFilmLibraryPage__buttonQueue');
 
 const createLibraryCardFunc = function(
   imgPath,
@@ -26,32 +30,11 @@ const createLibraryCardFunc = function(
   return card;
 };
 
-const favouriteButton = document.querySelector(
-  '.myFilmLibraryPage__buttonFavourites',
-);
-const queueButton = document.querySelector('.myFilmLibraryPage__buttonQueue');
-
 const drawWatchedFilmList = function() {
-  // favouriteButton.classList.add('button__colored');/**///////////////////////////////////////// */
   const fragment = document.createDocumentFragment();
-
-  localStorage.setItem('filmsWatched', [
-    {
-      id: 99861,
-      backdrop_path: '/rFtsE7Lhlc2jRWF7SRAU0fvrveQ.jpg',
-      original_title: 'Avengers: Age of Ultron',
-      vote_average: 7.3,
-    },
-    {
-      id: 228150,
-      vote_average: 7.4,
-      backdrop_path: '/pKawqrtCBMmxarft7o1LbEynys7.jpg',
-      original_title: 'Title 2',
-    },
-  ]); /*/*/ //////
-
-  const favouriteMovies = localStorage.getItem('filmsWatched');
-  if (favouriteMovies.length === 0 || favouriteMovies === null) {
+  const favouriteMovies = JSON.parse(localStorage.getItem('filmsWatched'));
+  console.log(favouriteMovies);
+  if (favouriteMovies === null || favouriteMovies.length === 0) {
     return alert('You do not have watched movies. Add them.');
   }
   favouriteMovies.forEach(el =>
@@ -66,7 +49,32 @@ const drawWatchedFilmList = function() {
   );
   films.innerHTML = '';
   films.append(fragment);
-  // films.addEventListener('click', (id, b) => activeDetailsPage(id, true));
+};
+
+const drawQueueFilmList = function() {
+  const fragment = document.createDocumentFragment();
+  const queueMovies = JSON.parse(localStorage.getItem('filmsQueue'));
+  console.log(queueMovies);
+  if (queueMovies === null || queueMovies.length === 0) {
+    return alert('You do not have to queue movies to watch. Add them.');
+  }
+  queueMovies.forEach(el =>
+    fragment.append(
+      createLibraryCardFunc(
+        el.backdrop_path,
+        el.original_title,
+        el.id,
+        el.vote_average,
+      ),
+    ),
+  );
+  films.innerHTML = '';
+  films.append(fragment);
 };
 
 favouriteButton.addEventListener('click', drawWatchedFilmList);
+queueButton.addEventListener('click', drawQueueFilmList);
+
+// films.addEventListener('click');
+
+// films.addEventListener('click', (id, b) => activeDetailsPage(id, true));
