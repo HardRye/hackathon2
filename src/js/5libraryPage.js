9cbca99b1a23d2c4ff73065ef062b441ac592171
Maksym Osadchuk <mosadchuk7@gmail.com>
2019-09-01 10:54:04 +0300

    logic first draft 01/09

--- src/js/5libraryPage.js
+++ src/js/5libraryPage.js
@@ -9,9 +9,12 @@ const createLibraryCardFunc = function(
   const card = document.createElement('li');
   card.classList.add('film');
   card.classList.add('data-id', movieId);
+
   const image = document.createElement('img');
-  image.setAttribute('src', imgPath);
-  const title = document.createElement('h2');
+  image.setAttribute('src', 'https://image.tmdb.org/t/p/' + 'w500' + imgPath);
+  image.setAttribute('alt', filmTitle);
+
+  const title = document.createElement('p');
   title.classList.add('myFilmLibraryPage__film-title');
   title.textContent = filmTitle;
   const rating = document.createElement('div');
@@ -20,7 +23,7 @@ const createLibraryCardFunc = function(
   card.appendChild(image);
   card.appendChild(title);
   card.appendChild(rating);
-  // card.addEventListener("click", activeDetailsPage(movieId, true))
+  return card;
 };
 
 const favouriteButton = document.querySelector(
@@ -30,14 +33,40 @@ const queueButton = document.querySelector('.myFilmLibraryPage__buttonQueue');
 
 const drawWatchedFilmList = function() {
   // favouriteButton.classList.add('button__colored');/**///////////////////////////////////////// */
-  let fragment;
-  const add = localStorage.setItem('filmsWatched', [1, 2, 3]); /*/*/ //////
+  const fragment = document.createDocumentFragment();
+
+  localStorage.setItem('filmsWatched', [
+    {
+      id: 99861,
+      backdrop_path: '/rFtsE7Lhlc2jRWF7SRAU0fvrveQ.jpg',
+      original_title: 'Avengers: Age of Ultron',
+      vote_average: 7.3,
+    },
+    {
+      id: 228150,
+      vote_average: 7.4,
+      backdrop_path: '/pKawqrtCBMmxarft7o1LbEynys7.jpg',
+      original_title: 'Title 2',
+    },
+  ]); /*/*/ //////
+
   const favouriteMovies = localStorage.getItem('filmsWatched');
   if (favouriteMovies.length === 0 || favouriteMovies === null) {
     return alert('You do not have watched movies. Add them.');
   }
-  fragment = favouriteMovies.map(el => createLibraryCardFunc(el));
-  films.innerHTML = fragment;
+  favouriteMovies.forEach(el =>
+    fragment.append(
+      createLibraryCardFunc(
+        el.backdrop_path,
+        el.original_title,
+        el.id,
+        el.vote_average,
+      ),
+    ),
+  );
+  films.innerHTML = '';
+  films.append(fragment);
+  // films.addEventListener('click', (id, b) => activeDetailsPage(id, true));
 };
 
 favouriteButton.addEventListener('click', drawWatchedFilmList);
